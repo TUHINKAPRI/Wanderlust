673a664e5e461c72bb0af785085bc8ceb0da1507
Tuhin <tuhinkapri2@gmail.com>
2023-12-22 20:56:48 +0530

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 function App() {
   return (
     <div>
-     hi dar
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/log-in" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
